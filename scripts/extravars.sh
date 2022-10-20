a2f8c9befcbcfe8e965947e5f81b2991596ccad3
James Park-Watt <jimmypw@users.noreply.github.com>
2022-10-20 09:57:33 +0100

    Pipeline promotions (#5)
    * Update Semaphore configuration
    
    * production pipeline must use production secrets
    
    * move letsencrypt issuer variables in to the pipeline and out of extravars
    reconfigure the deployment to use branches
    
    * prevent tagged commits from being pushed to preprod
    
    * fiddled with the semaphore auto promotion logic
    
    * perform rollout restart after deploying new manifests
    
    * remove erronious - when restarting deployments in preprod
    
    * specify that a deployment will be restarted

--- scripts/extravars.sh
+++ scripts/extravars.sh
@@ -7,16 +7,12 @@ export POSTGRES_PASSWORD_B64="$(echo -n ${POSTGRES_PASSWORD} | base64 -w 0)"
 export POSTGRES_USER_B64="$(echo -n ${POSTGRES_USER} | base64 -w 0)"
 export POSTGRES_DB_B64="$(echo -n ${POSTGRES_DB} | base64 -w 0)"
 
-if [ "${SEMAPHORE_GIT_BRANCH}" == "main" ]; then
-	export DEPLOYMENT_HOST="${SEMAPHORE_GIT_REPO_NAME}"
-	export LETSENCRYPT_ISSUER="letsencrypt-prod"
-	if [ ${ENVIRONMENT} == "PREPROD" ]; then
-		export K8S_RESOURCE_PREFIX="${SEMAPHORE_GIT_BRANCH//[^[:alnum:]]/-}-"
-	fi
-else
-	export DEPLOYMENT_HOST="${SEMAPHORE_GIT_BRANCH}.preprod.${SEMAPHORE_GIT_REPO_NAME}"
-	export LETSENCRYPT_ISSUER="letsencrypt-staging"
-	export K8S_RESOURCE_PREFIX="${SEMAPHORE_GIT_BRANCH//[^[:alnum:]]/-}-"
+if [ "${SEMAPHORE_GIT_TAG_NAME}x" == "x" ]; then # Not git tagged, destination preprod, use branch name
+ 	export K8S_RESOURCE_PREFIX="${SEMAPHORE_GIT_BRANCH//[^[:alnum:]]/-}-"
+ 	export DEPLOYMENT_HOST="${SEMAPHORE_GIT_BRANCH}.preprod.${SEMAPHORE_GIT_REPO_NAME}"
+else # git tagged destination prod, don't prefix objects, use prod url's
+ 	export K8S_RESOURCE_PREFIX=""
+ 	export DEPLOYMENT_HOST="${SEMAPHORE_GIT_REPO_NAME}"
 fi
 
 export DEPLOYMENT_URL="${DEPLOYMENT_HOST}.${TLD}"
